a6cba6237023bbb90d39c11e27f7bfd005476c6b
Daniel Isaac <trebolman@gmail.com>
2018-11-06 20:32:44 -0500

    Creacion de ejercicio3

---
+++ Clases-Ejercicio3/Clases/FiguraGeometrica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clases_Ejercicio3.Clases
+{
+    class FiguraGeometrica
+    {
+        public double CalcularArea(double valor1, double valor2)
+        {
+            return 
+        }
+        public double CalcularPerimetro()
+        {
+
+        }
+    }
+}
